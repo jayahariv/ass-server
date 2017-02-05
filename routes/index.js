e028f57e0629970b70abf13ddc05e917c9148ca3
Jayahari Vavachan <jvavachan@fb.com>
2017-02-04 22:18:02 -0800

    mentions added

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const http = require('http-request');
 
 const router = express.Router();
 
+const _401 = 'Please provide valid key & secret!';
 const _baseURL = 'http://api.assembla.com/v1'
 
 /* GET home page. */
@@ -13,22 +14,51 @@ router.get('/', function(req, res, next) {
 router.get('/activity', function(req, response, next) {
   const key = req.query['key'];
   const secret = req.query['secret'];
-  http.get(
-    {
-    	url: _baseURL + '/activity',
-      headers: {
-        'X-Api-Key': key,
-        'X-Api-Secret': secret,
+  if (key && secret && key.length > 0 && secret.length > 0) {
+    http.get(
+      {
+      	url: _baseURL + '/activity',
+        headers: {
+          'X-Api-Key': key,
+          'X-Api-Secret': secret,
+        },
       },
-    },
-    function (err, result) {
-    	if (result && result.code == 200) {
-        response.send(result.buffer.toString());
-    	} else {
-        response.send(err);
-    		return;
-      }
-  });
+      function (err, result) {
+      	if (result && result.code == 200) {
+          response.send(result.buffer.toString());
+      	} else {
+          response.send(err);
+      		return;
+        }
+    });
+  } else {
+    response.send(_401);
+    return;
+  }
 });
+
+router.get('/mentions', function(req, response, next) {
+  const key = req.query['key'];
+  const secret = req.query['secret'];
+  if (key && secret && key.length > 0 && secret.length > 0) {
+    http.get(
+      {
+      	url: _baseURL + '/user/mentions.json',
+        headers: {
+          'X-Api-Key': key,
+          'X-Api-Secret': secret,
+        },
+      },
+      function (err, result) {
+      	if (result && result.code == 200) {
+          response.send(result.buffer.toString());
+      	} else {
+          response.send(err);
+      		return;
+        }
+    });
+  }
+});
+
 
 module.exports = router;
